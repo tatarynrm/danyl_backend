76c008f952345ca0b3e527e582ae0e2252ab3635
tatarynrm <tatarynrm@gmail.com>
2024-06-04 14:01:39 +0300

    ewqeq

--- controllers/device_log.js
+++ controllers/device_log.js
@@ -6,7 +6,7 @@ class DeviceLogController {
   async saveDeviceLog(req, res, next) {
 
     const values = Object.values(req.query).map(Number); // Перетворюємо значення параметрів у числа
-    console.log(values);
+    console.log('VALUES',values);
     let client;
 
     try {
@@ -14,14 +14,14 @@ class DeviceLogController {
 
 const existRecord = await client.query(`select * from device_settings where device_code = ${values[0]}`)
 
-console.log(existRecord.rows);
 
+console.log(values[0]);
 
 if (existRecord.rows[0]) {
   console.log('EXIST',existRecord.rows);
   const query = `
   UPDATE device_settings 
-  SET  kupurnik = $2, monetnik = $3, bamk = $4, link_server = $5, water_type = $6, price1_1 = $7, price1_2 = $8, tara_calibrate1 = $9, tara_calibrate2 = $10, min_bak_1 = $11, max_bak_1 = $12, nosaleb1 = $13, min_bak_2 = $14, max_bak_2 = $15, nosaleb2 = $16, sensor_t_present1 = $17, sensor_t_min1 = $18, sensor_t_max1 = $19, sensor_t_present2 = $20, sensor_t_min2 = $21, sensor_t_max2 = $22, sensor_t_hand1 = $23, sensor_t_hand2 = $24, sensor_light_present = $25, sensor_light_on = $26, sensor_light_off = $27, sensor_light_hand_on_off = $28, sensor_power = $29, sensor_water_leaks = $30, sensor_water_in = $31, sensor_door_present = $32,display =$33
+  SET  display = $2,kupurnik = $3, monetnik = $4, bamk = $5, link_server = $6, water_type = $7, price1_1 = $8, price1_2 = $9, tara_calibrate1 = $10, tara_calibrate2 = $11, min_bak_1 = $12, max_bak_1 = $13, nosaleb1 = $14, min_bak_2 = $15, max_bak_2 = $16, nosaleb2 = $17, sensor_t_present1 = $18, sensor_t_min1 = $19, sensor_t_max1 = $20, sensor_t_present2 = $21, sensor_t_min2 = $22, sensor_t_max2 = $23, sensor_t_hand1 = $24, sensor_t_hand2 = $25, sensor_light_present = $26, sensor_light_on = $27, sensor_light_off = $28, sensor_light_hand_on_off = $29, sensor_power = $30, sensor_water_leaks = $31, sensor_water_in = $32, sensor_door_present = $33
   WHERE device_code = $1`
   await client.query("BEGIN"); // Початок транзакції
   await client.query(query, values);
@@ -35,10 +35,10 @@ if (existRecord.rows[0]) {
     }
   }
   const query = `INSERT INTO device_settings 
-  (device_code, kupurnik, monetnik, bamk, link_server, water_type, price1_1, price1_2, tara_calibrate1, tara_calibrate2, min_bak_1, max_bak_1, nosaleb1, min_bak_2, max_bak_2, nosaleb2, sensor_t_present1, sensor_t_min1, sensor_t_max1, sensor_t_present2, sensor_t_min2, sensor_t_max2, sensor_t_hand1, sensor_t_hand2, sensor_light_present, sensor_light_on, sensor_light_off, sensor_light_hand_on_off, sensor_power, sensor_water_leaks, sensor_water_in, sensor_door_present,display) 
+  (device_code,display, kupurnik, monetnik, bamk, link_server, water_type, price1_1, price1_2, tara_calibrate1, tara_calibrate2, min_bak_1, max_bak_1, nosaleb1, min_bak_2, max_bak_2, nosaleb2, sensor_t_present1, sensor_t_min1, sensor_t_max1, sensor_t_present2, sensor_t_min2, sensor_t_max2, sensor_t_hand1, sensor_t_hand2, sensor_light_present, sensor_light_on, sensor_light_off, sensor_light_hand_on_off, sensor_power, sensor_water_leaks, sensor_water_in, sensor_door_present) 
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32,$33)`;
     await client.query("BEGIN"); // Початок транзакції
-    await client.query(query, values);
+    await client.query(query, [...values]);
     await client.query("COMMIT"); // Підтвердження транзакції
     res.status(201).send("Data saved successfully");
 
