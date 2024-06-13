9ed7d6f99c3867d94b66181946997fc55012733f
tatarynrm <tatarynrm@gmail.com>
2024-06-13 23:07:08 +0300

    ewqeqw

--- index.js
+++ index.js
@@ -37,7 +37,12 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { maxAge: 1000 * 60 * 60 }
+  // 1 day
+  // cookie: { maxAge: 1000 * 60 * 60 } 
+
+
+  // 1 min
+  cookie: { maxAge: 1 * 60 * 1000 }
 }))
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
