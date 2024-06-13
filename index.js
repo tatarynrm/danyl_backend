bb735dcf6dc7b1ff923732d69f4beef6f421425b
tatarynrm <tatarynrm@gmail.com>
2024-06-13 19:21:28 +0300

    ewqewqewq

--- index.js
+++ index.js
@@ -211,8 +211,8 @@ app.get('/auth/google', googlePassport.authenticate('google', { scope: ['profile
 app.get('/auth/google/callback',
   googlePassport.authenticate('google', {
     
-    successRedirect:'https://vendwater.tech',
-    failureRedirect: '/login/failed',
+    successRedirect:'https://www.vendwater.tech/',
+    failureRedirect: 'https://www.vendwater.tech/login/failed',
   
   }));
 
