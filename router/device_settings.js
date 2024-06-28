32ca828005834b90a396818e86c7e2c000801dfe
tatarynrm <tatarynrm@gmail.com>
2024-06-28 11:46:33 +0300

    уцйу

--- router/device_settings.js
+++ router/device_settings.js
@@ -3,7 +3,7 @@ const router = new Router();
 const deviceLogController = require('../controllers/device_settings')
 const authMiddleware = require('../middlewares/auth-middlewares')
 
-router.get('/settings',deviceLogController.saveDeviceLog)
+router.get('/mainsettings',deviceLogController.saveDeviceLog)
 router.get('/settings/:id',deviceLogController.getOneDeviceLog)
 
 
