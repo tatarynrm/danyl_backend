d5e5dff513678620f38824aad64cb4e8a3ca7afa
tatarynrm <tatarynrm@gmail.com>
2024-06-28 11:26:32 +0300

    ewqe

--- controllers/device_settings.js
+++ controllers/device_settings.js
@@ -31,33 +31,34 @@ SET
   price_1 = $8,
   price_2 = $9,
   min_bak = $10,
-  nosale = $11,
-  sensor_present1 = $12,
-  sensor_t_min_1 = $13,
-  sensor_t_max_1 = $14,
-  sensor_t_present_2 = $15,
-  sensor_t_min_2 = $16,
-  sensor_t_max_2 = $17,
-  sensor_t_hand_1 = $18,
-  sensor_t_hand_2 = $19,
-  sensor_light_present = $20,
-  sensor_light_on = $21,
-  sensor_light_off = $22,
-  sensor_light_on_off = $23,
-  sensor_light_hand = $24,
-  sensor_power = $25,
-  sensor_water_leaks = $26,
-  sensor_water_in = $27,
-  sensor_door_present = $28,
-  impulse_per_litr1 = $29,
-  impulse_per_litr2 = $30,
-  sensor_humidity_present = $31,
-  sensor_humidity_on = $32,
-  sensor_humidity_off = $33,
-  timeout_reset_mode = $34,
-  banknote_multiplayer = $35,
-  coins_multiplayer = $36,
-  sale_mode = $37
+  max_bak = $11,
+  nosale = $12,
+  sensor_present1 = $13,
+  sensor_t_min_1 = $14,
+  sensor_t_max_1 = $15,
+  sensor_t_present_2 = $16,
+  sensor_t_min_2 = $17,
+  sensor_t_max_2 = $18,
+  sensor_t_hand_1 = $19,
+  sensor_t_hand_2 = $20,
+  sensor_light_present = $21,
+  sensor_light_on = $22,
+  sensor_light_off = $23,
+  sensor_light_on_off = $24,
+  sensor_light_hand = $25,
+  sensor_power = $26,
+  sensor_water_leaks = $27,
+  sensor_water_in = $28,
+  sensor_door_present = $29,
+  impulse_per_litr1 = $30,
+  impulse_per_litr2 = $31,
+  sensor_humidity_present = $32,
+  sensor_humidity_on = $33,
+  sensor_humidity_off = $34,
+  timeout_reset_mode = $35,
+  banknote_multiplayer = $36,
+  coins_multiplayer = $37,
+  sale_mode = $38
 WHERE device_id = $1;`
   await client.query("BEGIN"); // Початок транзакції
   await client.query(query, values);
@@ -70,10 +71,13 @@ WHERE device_id = $1;`
       params.push(req.query[i]);
     }
   }
-  const query = `INSERT INTO controller_settings 
-  (device_id,display,kupurnik, monetnik, bank, link_server, water_type, price_1, price_2, min_bak, nosale, sensor_present1, sensor_t_min_1, sensor_t_max_1, sensor_t_present_2, sensor_t_min_2, sensor_t_max_2, sensor_t_hand_1, sensor_t_hand_2, sensor_light_present, sensor_light_on, sensor_light_off, sensor_light_on_off,sensor_light_hand, sensor_power, sensor_water_leaks, sensor_water_in, sensor_door_present,impulse_per_litr1,impulse_per_litr2,sensor_humidity_present,sensor_humidity_on,sensor_humidity_off,timeout_reset_mode,banknote_multiplayer,coins_multiplayer,sale_mode) 
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32,$33,$34,$35,$36,$37)`;
-    
+const query = `
+INSERT INTO controller_settings 
+  (device_id, display, kupurnik, monetnik, bank, link_server, water_type, price_1, price_2, min_bak, max_bak, nosale, sensor_present1, sensor_t_min_1, sensor_t_max_1, sensor_t_present_2, sensor_t_min_2, sensor_t_max_2, sensor_t_hand_1, sensor_t_hand_2, sensor_light_present, sensor_light_on, sensor_light_off, sensor_light_on_off, sensor_light_hand, sensor_power, sensor_water_leaks, sensor_water_in, sensor_door_present, impulse_per_litr1, impulse_per_litr2, sensor_humidity_present, sensor_humidity_on, sensor_humidity_off, timeout_reset_mode, banknote_multiplayer, coins_multiplayer, sale_mode)
+VALUES 
+  ($2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39);
+
+`
   
   // const query = `INSERT INTO controller_settings 
   // (device_id) 
@@ -189,3 +193,5 @@ module.exports = new DeviceLogController();
 // };
 
 // ..finance/order?0=EWQ132131&1=20&10=30
+
+
